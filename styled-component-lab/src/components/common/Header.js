7e0a4661e04dfdd43e39077e6ee223d563a7d438
Nghi Le <vinhnghi223@gmail.com>
2022-02-15 02:01:18 +0200

    added media queries for menu

--- styled-component-lab/src/components/common/Header.js
+++ styled-component-lab/src/components/common/Header.js
@@ -13,15 +13,26 @@ const HeaderWrapper = styled.header`
 `
 
 const Menu = styled.nav`
+    // default mobile view
     display: flex;
-    position: relative;
-    width: initial;
-    border-bottom: none;
-    margin: auto 0 auto auto;
     font-family: 'Open Sans';
-    background: none;
-    left: initial;
-    top: initial;
+    position: absolute;
+    width: 100%;
+    top: 60px;
+    left: 0;
+    padding: 8px;
+    box-sizing: border-box;
+
+    @media (min-width: 768px){
+        display: flex;
+        position: relative;
+        width: initial;
+        border-bottom: none;
+        margin: auto 0 auto auto;
+        background: none;
+        left: initial;
+        top: initial;
+    }
 `
 
 // we dont want to pass isActive to the normal Link/ReactRouterDomLink component
